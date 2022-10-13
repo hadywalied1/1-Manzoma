a691c8d780202c70bfba67d6a415850c08f14c5b
Hady Walied <my.mepharo1230@gmail.com>
2022-10-13 21:47:29 +0200

    updated settings saving mech.

---
+++ GUI/settingsUI.py
@@ -0,0 +1,54 @@
+import sys
+from PySide2 import QtGui, QtCore, QtWidgets
+from PySide2.QtWidgets import (QMainWindow,
+                               QPushButton, QWidget, QLabel,
+                               QHBoxLayout, QVBoxLayout, QCheckBox,
+                               QProgressBar, QListWidget, QListWidgetItem,
+                               QTextEdit,QComboBox )
+from PySide2.QtGui import QFont, QIcon
+from PySide2.QtCore import Qt
+from Core.config import *
+
+import pathlib as path
+
+class Settings(QWidget):
+    def __init__(self):
+      super(Settings, self).__init__()
+
+      self.layout = QVBoxLayout()
+      self.layout.setAlignment(Qt.AlignRight | Qt.AlignAbsolute)
+      self.urlEdit = QTextEdit()
+      self.urlEdit.setPlaceholderText("enter Address")
+      self.urlEdit.setMaximumHeight(100)               
+      self.layout.addWidget(self.urlEdit)
+      
+      self.comEdit = QComboBox()
+      self.comEdit.addItems(serial_ports())
+      self.comEdit.setMaximumHeight(50)               
+      self.layout.addWidget(self.comEdit)
+      
+        
+      self.listWidget = QListWidget()
+      items = list(examIds.values())
+      for item_text in items:
+         item = QListWidgetItem(item_text)
+         self.listWidget.addItem(item)
+      self.config = getConfigFile() 
+      idx = list(examIds.keys()).index(self.config['examId']) 
+      item = self.listWidget.item(idx)
+      self.listWidget.setCurrentItem(item)
+      self.layout.addWidget(self.listWidget)
+        
+      self.bSubmit = QPushButton("حفظ")
+      self.bSubmit.clicked.connect(lambda: self._callback())
+      self.layout.addWidget(self.bSubmit)
+        
+      self.setLayout(self.layout)
+        
+   
+    def _callback(self):
+      url = self.urlEdit.toPlainText()
+      com = self.comEdit.currentData()
+      examid = self.listWidget.currentItem()
+      saveConfigFile(url, com, examid)
+
